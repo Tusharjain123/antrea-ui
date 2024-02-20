c0ac2d8a84c9297377f80fceb51e76b9cb1179be
Tushar Jain <jain0735@gmail.com>
2024-02-20 23:49:46 +0530

    chore: Added a card in Summary Page
    Signed-off-by: Tushar Jain <jain0735@gmail.com>

--- client/web/antrea-ui/src/routes/summary.tsx
+++ client/web/antrea-ui/src/routes/summary.tsx
@@ -162,6 +162,17 @@ export default function Summary() {
                 <WaitForAPIResource ready={agentInfos !== undefined} text="Loading Agents Information">
                     <ComponentSummary title="Agents" data={agentInfos!} propertyNames={agentProperties} getProperties={agentPropertyValues} />
                 </WaitForAPIResource>
+                <CdsCard title='Hello World'>
+                    <div cds-layout="vertical gap:md">
+                        <div cds-text="section" cds-layout="p-y:sm">
+                            Hello World
+                        </div>
+                        <CdsDivider cds-card-remove-margin></CdsDivider>
+                        <div cds-text="body" cds-layout="p-y:sm">
+                            My Github ID is Tusharjain123
+                        </div>
+                    </div>
+                </CdsCard>
             </div>
         </main>
     );
